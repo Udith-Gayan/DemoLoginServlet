d5b0a9fe23d164e9f91b3fa2d15dae4e8857dfa6
udith <udith.indrakantha@gmail.com>
2019-01-30 19:38:31 +0530

    Second add

---
+++ src/mvcdemo/model/User.java
@@ -0,0 +1,88 @@
+
+package mvcdemo.model;
+
+public class User {
+
+	private String username;
+	private String password;
+
+	public User(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+package mvcdemo.model;
+ 
+public class User {
+ 
+	private String username;
+	private String password;
+ 
+	public User(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+ 
+	public String getUsername() {
+		return username;
+	}
+ 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+ 
+	public String getPassword() {
+		return password;
+	}
+ 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+ 
+}
